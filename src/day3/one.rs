3985baae75ec324893e784540231591415324b07
Boxfort <jackwanderson@protonmail.com>
2019-12-04 13:00:43 +0000

    begin solution to day3-1

--- src/day3/one.rs
+++ src/day3/one.rs
@@ -1,11 +1,21 @@
 use super::ToPoints;
 
+fn get_intersections(a: &Vec<(i32, i32)>, b: &Vec<(i32, i32)>) -> Vec<(i32, i32)> {
+    // Compare all pairs of points in each line to see if there's an intersection
+    for i in 1..(a.len() - 1) {
+        for j in 1..(b.len() - 1) {
+        }
+    }
+
+    return vec!()
+}
+
 fn solve() -> i32 {
     // Get input points
     let input = super::get_input();
-    println!("{:?}", input[0].to_points());
 
     // Check all lines to see if they intersect, if so get the point that they intersect at
+    let intersections = get_intersections(&input[0].to_points(), &input[1].to_points());
 
     // Take all intersections and get their manhattan distance from (0,0)
 
